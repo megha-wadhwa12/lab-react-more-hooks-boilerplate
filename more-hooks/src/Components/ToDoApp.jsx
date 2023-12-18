3c9de5b4a19a1a49a7908c0b4bb3ee8d2e7233a6
megha-wadhwa12 <megha.wadhwa@kalvium.community>
2023-12-18 18:21:10 +0530

    done

---
+++ more-hooks/src/Components/ToDoApp.jsx
@@ -0,0 +1,56 @@
+import React, { useReducer, useRef } from 'react'
+import TodoItem from './TodoItem';
+
+const initialState = [
+    {
+        data: "First Item",
+        isHidden: false
+    }
+]
+
+const todoReducer = (state,action)=>{
+    console.log("action: ", action);
+    if(action.type == "ADD_ITEM"){
+        return [
+            ...state,{
+                data: action.payload,
+                isHidden: false
+            }
+        ]
+    }
+    if(action.type=="CHANGE_ISHIDDEN"){
+        return state.map((e,i)=>{
+            return i == action.payload ? {...e, isHidden: !e.isHidden} :e
+        })
+    }
+
+    return state
+}
+
+
+const ToDoApp = () => {
+
+    const [todo , dispatch] = useReducer(todoReducer , initialState)
+    console.log("todo: ", todo);
+
+    const input = useRef(null)
+
+  return (
+    <div>
+        <input ref={input} type="text" onKeyDown={(e)=>{
+            if(e.key=="Enter"){
+                dispatch({type: "ADD_ITEM",payload: e.target.value})
+            }
+        }}/>
+        {todo.map((e,i)=>{
+            return <TodoItem item={e} index={i} dispatch={dispatch}/>
+        })}
+        <button onClick={()=>{
+            input.current.focus()
+            // window.scrollTo(top:0,behaviour:)
+        }}>GO BACK TO TOP</button>
+    </div>
+  )
+}
+
+export default ToDoApp
---
+++ more-hooks/src/Components/TodoItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const TodoItem = ({item: {data,isHidden} ,index,dispatch}) => {
+    console.log("isHidden: ", isHidden);
+  return (
+    <div style={{
+        backgroundColor: "purple",
+        width: "90vw",
+        filter: `${isHidden?"blur(2px)":"blur(0px)"}`
+    }}>
+        <h3>{isHidden ? "This Content is Hidden": data}</h3>
+        <button onClick={()=>{
+            dispatch({type: "CHANGE_ISHIDDEN" , payload: index})
+        }}>Toggle</button>
+    </div>
+  )
+}
+
+export default TodoItem
